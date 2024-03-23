4a07467748b2afca65fa4a98190cd710cfcacf5a
milohill <116620023+milohill@users.noreply.github.com>
2024-03-23 16:32:04 -0500

    make unneeded commands comments

--- macOS.sh
+++ macOS.sh
@@ -10,16 +10,16 @@ read
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false && killall Finder
 
 # Get the absolute path to the image
-IMAGE_PATH="${HOME}/dotfiles/settings/Desktop.png"
+# IMAGE_PATH="${HOME}/dotfiles/settings/Desktop.png"
 
 # AppleScript command to set the desktop background
-osascript <<EOF
-tell application "System Events"
-    set desktopCount to count of desktops
-    repeat with desktopNumber from 1 to desktopCount
-        tell desktop desktopNumber
-            set picture to "$IMAGE_PATH"
-        end tell
-    end repeat
-end tell
-EOF
+# osascript <<EOF
+# tell application "System Events"
+#     set desktopCount to count of desktops
+#     repeat with desktopNumber from 1 to desktopCount
+#         tell desktop desktopNumber
+#             set picture to "$IMAGE_PATH"
+#         end tell
+#     end repeat
+# end tell
+# EOF
